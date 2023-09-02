75e7c10016699a18c57d19a15b91f73958a01da7
root <root@work>
2023-09-02 03:39:35 -0600

    Redo project to include search and multi descriptions.

--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -1,41 +1,13 @@
 import { useState } from 'react'
-import React from 'react'
-import axios from 'axios'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  //const url = 'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid=1426b5d17bc08ef6367a71fb5dc0b879'
-
+  //const url = 'https://api.openweathermap.org/data/2.5/weather?q=SanFrancisco&appid=1426b5d17bc08ef6367a71fb5dc0b879'
   return (
     <>
-    <div className="App">
-      <div className="card">
-        <div className="top">
-          <div className="location">
-            <p>Millbrae</p>
-          </div>
-          <div className="temp">
-            <h1>65°</h1>
-          </div>
-          <div className="description">
-            <p>cloudy</p>
-          </div>
-        </div>
-        <div className="bottom">
-          <div className="feels">
-            <p>65°F</p>
-          </div>
-          <div className="humidity">
-            <p>20°</p>
-          </div>
-          <div className="wind">
-            <p>12 mph nw</p>
-          </div>
-        </div>
-      </div>
-    </div>
+
     </>
   )
 }
